694f2728a6c6a39759c7265002b765ffbf6ff4d9
aragon-cdpr <rafal.tomaszewski@codeperium.io>
2024-02-29 02:42:58 +0100

    Initial svelte project, changed main page

---
+++ src/src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<section>
+	<div>
+		<img src="https://codeperium.s3.eu-west-1.amazonaws.com/assets/images/Logo/RGB/pozioma/SVG/codeperium_logo_white_rgb.svg" alt="logo" />
+		<h3> Work in progress...</h3>
+		<a href="https://linktr.ee/codeperium"> Check important links!</a>
+	</div>
+</section>
+
+<style>
+	section {
+		width: 100vw;
+		height: 100vh;
+		padding: 0px 50px;
+		background-color: #393C47;
+		color: #F2F6F9;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	section div {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		width: 1500px;
+	}
+	section div h3 {
+		display: inline-block;
+		font-size: 32px;
+    margin-bottom: 30px;
+	}
+	section div a {
+		display: block;
+		text-decoration: none;
+		color: #F2F6F9;
+		font-weight: bold;
+		font-size: 24px;
+		padding: 15px 60px;
+		border-radius: 30px;
+		background-color: #D0A45C;
+	}
+
+	section div a:hover {
+		color: #393C47;
+		background-color: #FF1F30;
+	}
+
+	section div img {
+		display: block;
+		width: 100%;
+    margin-bottom: 70px;
+	}
+	
+</style>
+
