ec1c7eed1de6cdd5652b4450a55672c23764be37
aragon-cdpr <rafal.tomaszewski27@gmail.com>
2024-07-09 23:03:42 +0200

    change initialization of dataLayer

--- src/src/lib/gtm.svelte.ts
+++ src/src/lib/gtm.svelte.ts
@@ -2,15 +2,14 @@ import { onMount } from "svelte";
 
 type Event = {
     event: string,
-    [key: string]: string
+    [key: string]: string,
 }
 
-export const dataLayer: any[] = $state([]); 
-
-if (typeof window !== 'undefined' && !window.google_tag_manager) {
-    dataLayer.push(window.dataLayer || []);
-
+if (typeof window !== 'undefined') {
+    window.dataLayer = window.dataLayer || [];
 }
+
+export const dataLayer: any[] = $state(typeof window !== 'undefined' ? window.dataLayer : []); 
 
 export const pushEvent = (eventObj: Event) => {
     dataLayer.push(eventObj);
@@ -18,13 +17,12 @@ export const pushEvent = (eventObj: Event) => {
 
 export const initGTM = (gtag: string)  => {
     onMount(() => {
-        window.dataLayer = window.dataLayer || [];
         const parentScript = document.getElementsByTagName('script')[0];
         const script = document.createElement('script');
         script.async = true;
         script.src = 'https://www.googletagmanager.com/gtm.js?id=' + gtag;
         script.parentNode?.insertBefore(script, parentScript);
 
-        window.dataLayer.push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+        pushEvent({ 'gtm.start': new Date().getTime().toString(), event: 'gtm.js' });
     })
 }
