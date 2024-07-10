892626517d17f6db51c52849b8e7b1e53718a64d
aragon-cdpr <rafal.tomaszewski27@gmail.com>
2024-07-10 02:11:15 +0200

    map releases with sentry

--- src/src/hooks.client.ts
+++ src/src/hooks.client.ts
@@ -1,8 +1,9 @@
 import * as Sentry from "@sentry/sveltekit";
+import {PUBLIC_ENV, PUBLIC_SENTRY_DSN} from "$env/static/public";
 
 Sentry.init({
-    environment: process.env.ENV,
-    dsn: process.env.SENTRY_DSN,
+    environment: PUBLIC_ENV,
+    dsn: PUBLIC_SENTRY_DSN,
 
     // We recommend adjusting this value in production, or using tracesSampler
     // for finer control
