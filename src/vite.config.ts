200bb21222d859a61bf2031e4b5eb7adbbda95d4
aragon-cdpr <rafal.tomaszewski@codeperium.io>
2024-07-10 06:56:57 +0200

    Feature/cod frontend new layout (#30)
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -7,15 +7,12 @@ export default ( mode: string) => {
 
 	process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 	return defineConfig({
-		plugins: [sentrySvelteKit({
-			adapter: 'vercel',
-			sourceMapsUploadOptions: {
-			  org: process.env.PUBLIC_SENTRY_ORG  ?? process.env.VITE_CODEPERIUM,
-			  project: process.env.PUBLIC_SENTRY_PROJECT ?? process.env.VITE_CODEPERIUM,
-			  authToken: process.env.SENTRY_AUTH_TOKEN ?? process.env.VITE_SENTRY_AUTH_TOKEN,
-			  release: {name: 'codeperium@' + process.env.npm_package_version}
-			},
-		  }), sveltekit()],
+		build: {
+			sourcemap: true
+		},
+		plugins: [sveltekit(), sentrySvelteKit({
+			adapter: 'vercel'
+		  })],
 });
 
 };
