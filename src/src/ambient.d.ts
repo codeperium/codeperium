a160b644266491bf272c06f6784532a4ade77c34
aragon-cdpr <rafal.tomaszewski@codeperium.io>
2024-07-17 01:26:52 +0200

    run eslinter and prettier

--- src/src/ambient.d.ts
+++ src/src/ambient.d.ts
@@ -1,42 +1,38 @@
-
 type Implements<T, U extends T> = U;
 
 export type logoType = {
-    logo: string | void;
-    logomark: string | void;
-    logoWhite: string | void;
-}
+	logo: string | void;
+	logomark: string | void;
+	logoWhite: string | void;
+};
 
 //ICONS
 type TArrowIconVariants = 'up' | 'down' | 'left' | 'right';
 type TIconType = 'arrow' | 'arrow-double' | 'squares' | 'slash' | 'socials';
 type TIconVariantsForType = {
-    arrow: TArrowIconVariants;
-    'arrow-double': TArrowIconVariants;
-    slash: 'back' | 'for';
-}
-type TIconVariant<T extends TIconType> = TIconVariantsForType[T]; 
+	arrow: TArrowIconVariants;
+	'arrow-double': TArrowIconVariants;
+	slash: 'back' | 'for';
+};
+type TIconVariant<T extends TIconType> = TIconVariantsForType[T];
 
 type TBaseIcon = {
-    name: string;
-    url: string | void;
-    color: IColor;
-}
+	name: string;
+	url: string | void;
+	color: IColor;
+};
 
 export type Icon<T extends TIconType = IconType> = TBaseIcon & {
-    type: T;
-    variant: TIconVariant<T>;
-}
-
+	type: T;
+	variant: TIconVariant<T>;
+};
 
 // COLORS
 type TColorVariants = 'default' | 'light' | 'dark';
 type TColorNames = 'red' | 'gold' | 'white' | 'grey' | 'black';
 
 interface IColor {
-    name: TColorNames;
-    variant: TColorVariants;
-    value: string;
+	name: TColorNames;
+	variant: TColorVariants;
+	value: string;
 }
-
-
--- src/src/lib/index.ts
+++ src/src/lib/index.ts
@@ -1,4 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
-import { dataLayer, initGTM, pushEvent } from "./gtm.svelte";
+import { dataLayer, initGTM, pushEvent } from './gtm.svelte';
 
-export {dataLayer, initGTM, pushEvent };
+export { dataLayer, initGTM, pushEvent };
