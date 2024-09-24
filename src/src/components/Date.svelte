042e902832bc25304462c5b432e09d995c1b3a6b
aragon-cdpr <rafal.tomaszewski@codeperium.io>
2024-09-24 21:18:46 +0200

    Staging (#50)
    * prismic integration, add slices and page types (#47)
    
    * improve GTM script loading, fully responsive roadmap section (#49)
    
    * prismic integration, add slices and page types
    
    * fully responsive roadmap section
    
    * improve roadmap position on desktop
    
    * repair GTM script loading

---
+++ src/src/components/Date.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import type { DateField } from '@prismicio/client';
+	let { startDate, statusColor }: { startDate: DateField; statusColor: String } = $props();
+
+	const date = new Date(startDate as string);
+	date.setDate(date.getDate() + 1);
+	const formatted = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short' }).format(
+		date
+	);
+</script>
+
+<p
+	class="xl:ml-[24px] ml-[12px] mr-[4px] flex after:w-[25px] lg:after:block lg:after:h-[1px] lg:after:bg-grey-light/10 lg:after:mt-[19px] relative lg:after:absolute lg:after:-right-[30px] lg:after:-top-[9px]"
+>
+	<span
+		class="block w-[20px] h-[20px] before:icon before:icon-ar-l before:icon-red before:icon-sm relative {statusColor}"
+	></span>
+	<span class="leading-[20px] text-red date {statusColor}">{formatted}</span>
+	<span
+		class="block w-[20px] h-[20px] -mr-[5px] before:icon before:icon-sl-f before:icon-red before:icon-sm relative {statusColor}"
+	></span>
+	<span
+		class="block w-[20px] h-[20px] before:icon before:icon-ar-r before:icon-red before:icon-sm relative {statusColor}"
+	></span>
+</p>
+
+<style>
+	.finished {
+		&.date {
+			@apply text-red;
+		}
+		&.status {
+			@apply bg-red;
+		}
+		&::after {
+			@apply icon-red;
+		}
+		&::before {
+			@apply icon-red;
+		}
+	}
+	.in-progress {
+		&.date {
+			@apply text-gold;
+		}
+		&.status {
+			@apply bg-gold;
+		}
+		&::after {
+			@apply icon-gold;
+		}
+		&::before {
+			@apply icon-gold;
+		}
+	}
+	.not-started {
+		@apply text-grey-light;
+		&.date {
+			@apply text-grey-light;
+		}
+		&.status {
+			@apply bg-grey-light;
+		}
+		&::after {
+			@apply icon-grey-light;
+		}
+		&::before {
+			@apply icon-grey-light;
+		}
+	}
+</style>
--- src/src/components/index.ts
+++ src/src/components/index.ts
@@ -3,5 +3,6 @@ import Main from './Main.svelte';
 import Footer from './Footer.svelte';
 import HeroHeader from './HeroHeader.svelte';
 import Cursor from './Cursor.svelte';
+import Date from './Date.svelte';
 
-export { Header, Main, Footer, HeroHeader, Cursor };
+export { Header, Main, Footer, HeroHeader, Cursor, Date };
